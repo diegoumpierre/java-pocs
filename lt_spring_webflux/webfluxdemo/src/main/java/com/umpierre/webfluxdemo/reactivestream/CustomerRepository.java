a05eace3a8ee1771b0839a1e79ef027196ff0cd7
dumpierre <dumpierre@lendingclub.com>
2024-02-08 15:02:10 -0300

    organize the repo

--- lt_spring_webflux/webfluxdemo/src/main/java/com/umpierre/webfluxdemo/reactivestream/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.umpierre.webfluxdemo.reactivestream;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
-    Flux<Customer> findAll();
-}
